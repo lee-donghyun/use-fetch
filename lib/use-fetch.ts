e42574861f01b68799ac6c4ce4616449c75a26b3
Donghyun Lee <leedonghyun022@gmail.com>
2024-07-26 14:28:13 +0900

    feat: null 일때 처리

--- lib/use-fetch.ts
+++ lib/use-fetch.ts
@@ -7,40 +7,46 @@ export const useFetch = <Data, Error>(
   key: Key | null,
   option: Option<Data, Error> = { keepPreviousData: true },
 ) => {
-  const [data, setData] = useState<Data | null>(null);
-  const [error, setError] = useState<Error | null>(null);
-  const [time, setTime] = useState(0);
   const keyRef = useRef<Key | null>(null);
   const freshPromiseId = useRef(0);
   const timeRef = useRef(0);
+
+  const [data, setData] = useState<Data | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [time, setTime] = useState(0);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useLayoutEffect(() => {
     if (keyRef.current !== key || timeRef.current !== time) {
       keyRef.current = key;
       timeRef.current = time;
+
+      if (key === null) {
+        setData(null);
+        setError(null);
+        return;
+      }
+
       if (!option.keepPreviousData) {
         setData(null);
         setError(null);
       }
-      if (key) {
-        const id = ++freshPromiseId.current;
-        api<Data>(key)
-          .then((data) => {
-            if (freshPromiseId.current === id) {
-              setData(data);
-              setError(null);
-              option.onSuccess?.(data);
-            }
-          })
-          .catch((error) => {
-            if (freshPromiseId.current === id) {
-              setData(null);
-              setError(error as Error);
-              option.onError?.(error as Error);
-            }
-          });
-      }
+      const id = ++freshPromiseId.current;
+      api<Data>(key)
+        .then((data) => {
+          if (freshPromiseId.current === id) {
+            setData(data);
+            setError(null);
+            option.onSuccess?.(data);
+          }
+        })
+        .catch((error) => {
+          if (freshPromiseId.current === id) {
+            setData(null);
+            setError(error as Error);
+            option.onError?.(error as Error);
+          }
+        });
     }
   });
 
